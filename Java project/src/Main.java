020849f56e33a821f07da8a8e95b8683e3d719e6
nagabharathchintakunta <chintakuntanagabharath22@gmail.com>
2023-06-01 21:18:24 -0500

    Updated

---
+++ Java project/src/Main.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[] args) {
+        Student obj = new Student();
+        obj.setName("Naga Bharath");
+        System.out.print(obj.getName());
+    }
+}
---
+++ Java project/src/Student.java
@@ -0,0 +1,10 @@
+class Student{
+    private String name;
+
+    public String getName(){
+        return name;}
+    public void setName(String N){
+        this.name = N;
+    }
+
+}
