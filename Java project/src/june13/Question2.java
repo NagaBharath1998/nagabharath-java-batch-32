4d71e5ed60754213125f4e8730642b4de26c05ee
nagabharathchintakunta <chintakuntanagabharath22@gmail.com>
2023-06-14 15:43:56 -0500

    June13 Assignment

---
+++ Java project/src/june13/Question2.java
@@ -0,0 +1,17 @@
+package june13;
+class NumberRunnable implements Runnable {
+    public void run() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
+public class Question2 {
+    public static void main(String[] args) {
+        NumberRunnable numberRunnable = new NumberRunnable();
+        Thread thread = new Thread(numberRunnable);
+        thread.start();
+    }
+}
+
