a83c76b494526b13b64412fc2404fa56edec76bd
nagabharathchintakunta <chintakuntanagabharath22@gmail.com>
2023-06-16 15:18:18 -0500

    June14 Assignment

--- Java project/src/june14/TicketBooking.java
+++ Java project/src/june14/TicketBooking.java
@@ -9,14 +9,14 @@ public class TicketBooking {
     private static Lock lock = new ReentrantLock();
 
     public static void main(String[] args) {
-        // Create multiple threads representing ticket buyers
+
         Thread[] threads = new Thread[5];
         for (int i = 0; i < threads.length; i++) {
             threads[i] = new Thread(new TicketBuyer(), "Buyer " + (i + 1));
             threads[i].start();
         }
 
-        // Wait for all threads to finish
+
         for (Thread thread : threads) {
             try {
                 thread.join();
